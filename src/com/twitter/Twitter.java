368eb99de137ce303e3d0e6a9429e5bdae6f4c16
janadjurovic <djurovic.jana@outlook.com>
2016-04-04 21:10:44 +0200

    Dodata JavaDoc dokumentacija za klasu Twitter, njene atribute i metode i generisana JavaDoc HTML strana

---
+++ src/com/twitter/Twitter.java
@@ -0,0 +1,70 @@
+package com.twitter;
+
+import java.util.LinkedList;
+import com.twitter.poruke.TwitterPoruka;
+/**
+ * Klasa Twitter sadrzi listu objekata TwitterPoruka i metode vratiSvePoruke(), unesi() i
+ * vratiPoruke() 
+ * @author Jana Djurovic
+ *
+ */
+public class Twitter {
+	/**
+	 * Atribut poruke je lista objekata TwitterPoruka
+	 */
+	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
+
+	/**
+	 * Metoda koja vraca listu poruka
+	 * @return poruke
+	 */
+	public LinkedList<TwitterPoruka> vratiSvePoruke() {
+		return poruke;
+	}
+	/**
+	 * Metoda za unos nove poruke u listu koja kao parametre prima stringove
+	 * @param korisnik
+	 * @param poruka
+	 */
+
+	public void unesi(String korisnik, String poruka) {
+		// Pravi se nova poruka i puni podacima.
+		TwitterPoruka tp = new TwitterPoruka();
+		tp.setKorisnik("korisnik");
+		tp.setPoruka(poruka);
+		// Poruka se unosi u listu na kraj
+		poruke.addLast(tp);
+	}
+	 /**
+	  * Metoda koja vraca sve poruke koje sadrze odredjeni tag i prima String tag i integer mexVroj
+	  * @param maxBroj
+	  * @param tag
+	  * @return rezultat (niz poruka koje sadrze zadati tag)
+	  * @throws RuntimeException u slucaju da je uneseni String prazan ili null
+	  */
+	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
+		if (tag == null || tag == "")
+			throw new RuntimeException("Morate uneti tag");
+		// Ako je maxBroj <=0, vraca maxBroj se postavlja na 100 poruka
+		if (maxBroj <= 0)
+			maxBroj = 100;
+		// Pomocna promenljiva koja predstavlja brojac upisanih poruka
+		int brojac = 0;
+		// Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
+		// sve poruke koje u sebi imaju zadati tag
+		TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj];
+		// Pretrazuju se poruke i traze se one koje sadrze tag.
+		// Ako se nadje neka takva, i ako nije prekoracen maxBroj
+		// ona se upisuje u niz. Ako je prekoracen maxBroj,pretraga
+		// se prekida.
+		for (int i = 0; i < poruke.size(); i++)
+			if (poruke.get(i).getPoruka().indexOf(tag) != -1)
+				if (brojac < maxBroj) {
+					rezultat[brojac + 1] = poruke.get(i);
+					brojac++;
+				} else
+					break;
+		return rezultat;
+	}
+
+}
